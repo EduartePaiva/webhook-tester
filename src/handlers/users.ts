98e7ded68c0f86e4a5e062ae416adafc6453e2eb
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-06-24 17:57:26 -0300

    added bun to watch the files change

---
+++ src/handlers/users.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from "express";
+
+export function getUsers(request: Request, response: Response) {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import usersRouter from "./routes/users";
+const PORT = 3000;
 
 const app = express();
+app.use("/api/users", usersRouter);
 
-const PORT = 3000;
+app.get("/api/users", (request, response, next) => {});
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
