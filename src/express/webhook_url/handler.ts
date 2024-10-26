d8f8709714d3a51377403b3acedfb149476814b3
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2024-10-26 07:12:44 -0300

    fix: wronk webhook url

--- src/express/webhook_url/handler.ts
+++ src/express/webhook_url/handler.ts
@@ -3,5 +3,5 @@ import { SocketData } from "../../types";
 
 export function getWebhookUrl(req: Request<any, any, { user: SocketData }>, res: Response) {
     const userId = req.body.user.id;
-    return res.send(`https://webhook.eduartepaiva.com/${userId}`);
+    return res.send(`https://webhook.eduartepaiva.com/api/message/${userId}`);
 }
